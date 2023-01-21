45374017b207c6cb0abb60f05b96a50c5c1de235
femi <oluwafemi.akinyemi@datamellon.com>
2023-01-21 09:50:37 +0100

    Summarize form

--- textsummarization/src/App.js
+++ textsummarization/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,23 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <form  >
+
+        <textarea placeholder='Put your text' />
+
+
+
+        <button>Summarize</button>
+
+
+
+
+      </form>
+
+
+
     </div>
   );
 }
