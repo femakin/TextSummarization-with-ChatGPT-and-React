75b3f6d4090d8779ce77632c223eb651e0ea9d8f
femi <oluwafemi.akinyemi@datamellon.com>
2023-01-22 18:47:57 +0100

    Clean up

--- textsummarization/src/App.js
+++ textsummarization/src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import { Configuration, OpenAIApi } from 'openai';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function App() {
 
@@ -10,64 +9,25 @@ function App() {
 
   const configuration = new Configuration({
     // apiKey: process.env.OPENAI_API_KEY,
+    apiKey: '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'
   });
   const openai = new OpenAIApi(configuration);
 
 
-  // console.log(openai, 'apenaiii')
-
   const HandleSubmit = (e) => {
     e.preventDefault()
-    // console.log(text)
-    console.log(openai.createCompletion({
+    openai.createCompletion({
       model: 'text-davinci-003',
       prompt: generatePrompt(text),
       temperature: 0.6,
       max_tokens: 100,
     }).then((res) => {
-      console.log(res?.data?.choices[0]?.text, 'res')
       setsummarizedtext(res?.data?.choices[0]?.text)
     })
-    )
   }
 
 
-
-  useEffect(() => {
-
-
-
-  }, [])
-
-
-
-  // export default async function (req, res) {
-  //   const { text } = req.body;
-  //   const completion = await openai.createCompletion({
-  //     model: 'text-davinci-003',
-  //     prompt: generatePrompt(text),
-  //     temperature: 0.6,
-  //     max_tokens: 2048,
-  //   });
-  //   res.status(200).json({ result: completion.data.choices[0].text });
-  // }
-
-  //   const getDetails = async (req, res)=>{
-  //   const { text } = req.body;
-  //   const completion = await openai.createCompletion({
-  //     model: 'text-davinci-003',
-  //     prompt: generatePrompt(text),
-  //     temperature: 0.6,
-  //     max_tokens: 2048,
-  //   });
-  //   res.status(200).json({ result: completion.data.choices[0].text });
-  // }
-
-
-
-
   function generatePrompt(text) {
-    // return `Summarize this text into five lines  ${text}.`;
     return `Summarize this text into five lines  ${text}. and break them into seperate lines`;
   }
 
@@ -75,30 +35,20 @@ function App() {
 
   return (
     <div className="App_">
-
-
       <div className='header'  >
         <h1 className='header_text' >Text  <span className='text_active'  >Summarizer</span></h1>
         <h2 className='header_summary' > Summarise your text into a shorter length.</h2>
       </div>
-
-
-
       <div className='container' >
         <div className='text_form'  >
           <form   >
             <label>Enter your text</label>
             <textarea rows={14} cols={80} placeholder='Put your text' value={text} onChange={(e) => setText(e.target.value)} />
-
           </form>
         </div>
-
-
         <div>
           <button type='button' onClick={HandleSubmit}  >Summarize</button>
         </div>
-
-
         <div className='summarized_text' >
           <label>Summarized text</label>
           <textarea placeholder='Summarized text' cols={80} rows={14} value={summarizedtext} onChange={(e) => setText(e.target.value)} />
